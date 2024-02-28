2b7edfe9310efa7cc462aa134297a65e86657b40
harshmangalam <harshdev8218@gmail.com>
2024-02-28 11:56:21 +0530

    added active route hook

---
+++ src/hooks/use-active-route.tsx
@@ -0,0 +1,23 @@
+import { useLocation } from "@builder.io/qwik-city";
+
+export const useActiveRoute = (href?: string) => {
+  const location = useLocation();
+  const toPathname = href ?? "";
+  const locationPathname = location.url.pathname;
+
+  const startSlashPosition =
+    toPathname !== "/" && toPathname.startsWith("/")
+      ? toPathname.length - 1
+      : toPathname.length;
+  const endSlashPosition =
+    toPathname !== "/" && toPathname.endsWith("/")
+      ? toPathname.length - 1
+      : toPathname.length;
+  const isActive =
+    locationPathname === toPathname ||
+    (locationPathname.endsWith(toPathname) &&
+      (locationPathname.charAt(endSlashPosition) === "/" ||
+        locationPathname.charAt(startSlashPosition) === "/"));
+
+  return isActive;
+};
