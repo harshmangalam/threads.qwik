c877c82c462224a8d086f92feaea76cc5f0f910d
harshmangalam <harshdev8218@gmail.com>
2024-02-28 10:58:22 +0530

    added logo component

---
+++ src/components/logo.tsx
@@ -0,0 +1,8 @@
+import { component$ } from "@builder.io/qwik";
+import { Link } from "@builder.io/qwik-city";
+import LogoIcon from "~/components/icons/logo.svg?jsx";
+export const Logo = component$(() => (
+  <Link href="/">
+    <LogoIcon class="h-8 w-8" />
+  </Link>
+));
