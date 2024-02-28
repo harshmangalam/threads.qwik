9e0562b541ea58d3e1aee4e0be7c802bcff392ff
harshmangalam <harshdev8218@gmail.com>
2024-02-28 15:04:41 +0530

    added nav dropdown

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,11 +1,12 @@
 import { component$ } from "@builder.io/qwik";
 import { Logo } from "../logo";
 import { NavLinks } from "./nav-links";
+import { NavDropdown } from "./nav-dropdown";
 
 export const Navbar = component$(() => {
   return (
     <header class="">
-      <nav class="mx-auto h-[74px] w-full max-w-[1230px]">
+      <nav class="container mx-auto h-[74px] w-full">
         <div class="grid h-full grid-cols-12">
           <section class=" col-span-3 flex items-center">
             <Logo />
@@ -15,7 +16,9 @@ export const Navbar = component$(() => {
             <NavLinks />
           </section>
 
-          <section class=" col-span-3"></section>
+          <section class=" col-span-3 flex items-center justify-end">
+            <NavDropdown />
+          </section>
         </div>
       </nav>
     </header>
---
+++ src/components/navbar/nav-dropdown.tsx
@@ -0,0 +1,36 @@
+import { component$ } from "@builder.io/qwik";
+import { Link } from "@builder.io/qwik-city";
+import MoreIcon from "~/assets/icons/more.svg?jsx";
+
+const NavLink = component$(
+  ({ href, label }: { href: string; label: string }) => (
+    <li>
+      <Link class="block w-full px-4 py-1 font-medium" href={href}>
+        {label}
+      </Link>
+    </li>
+  ),
+);
+export const NavDropdown = component$(() => {
+  return (
+    <div class="dropdown dropdown-end">
+      <div tabIndex={0} role="button" class="m-1 opacity-40 hover:opacity-100">
+        <MoreIcon />
+      </div>
+      <ul
+        tabIndex={0}
+        class="dropdown-content z-[1] mt-4 w-44  rounded-xl   bg-base-100 shadow-md last:pb-2"
+      >
+        <NavLink href="/" label="Appearance" />
+        <div class="divider m-0"></div>
+        <NavLink href="/" label="Settings" />
+        <div class="divider m-0"></div>
+        <NavLink href="/" label="Saved" />
+        <div class="divider m-0"></div>
+        <NavLink href="/" label="Your likes" />
+        <div class="divider m-0"></div>
+        <NavLink href="/" label="Logout" />
+      </ul>
+    </div>
+  );
+});
