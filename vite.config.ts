9426ad7c7c89995f7b79c6b892fdf2c240cc8e93
harshmangalam <harshdev8218@gmail.com>
2024-03-05 15:14:07 +0530

    setup auth

--- vite.config.ts
+++ vite.config.ts
@@ -16,5 +16,8 @@ export default defineConfig((): UserConfig => {
         "Cache-Control": "public, max-age=600",
       },
     },
+    optimizeDeps:{
+      include:["@auth/core"]
+    }
   };
 });
