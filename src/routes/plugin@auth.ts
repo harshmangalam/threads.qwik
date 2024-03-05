84ea227ffc22ebf4924cb71e671d452cdf2b210a
harshmangalam <harshdev8218@gmail.com>
2024-03-05 15:32:06 +0530

    added prisma schema

--- src/routes/plugin@auth.ts
+++ src/routes/plugin@auth.ts
@@ -1,9 +1,13 @@
 import { serverAuth$ } from "@builder.io/qwik-auth";
 import GitHub from "@auth/core/providers/github";
 import type { Provider } from "@auth/core/providers";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "~/utils/prisma";
 
 export const { onRequest, useAuthSession, useAuthSignin, useAuthSignout } =
   serverAuth$(({ env }) => ({
+    // @ts-ignore
+    adapter: PrismaAdapter(prisma),
     secret: env.get("AUTH_SECRET"),
     trustHost: true,
     providers: [
