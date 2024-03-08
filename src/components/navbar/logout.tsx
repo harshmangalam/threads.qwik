e582b6d3db0477c5d92c347b0d0f773fd2cce53e
harshmangalam <harshdev8218@gmail.com>
2024-03-08 19:18:36 +0530

    added thread schema

--- src/components/navbar/logout.tsx
+++ src/components/navbar/logout.tsx
@@ -2,6 +2,7 @@ import { component$ } from "@builder.io/qwik";
 import { Form } from "@builder.io/qwik-city";
 import { useAuthSignout } from "~/routes/plugin@auth";
 
+// change ONLINE status to OFFLINE after logout in DB
 export const Logout = component$(() => {
   const logout = useAuthSignout();
   return (
