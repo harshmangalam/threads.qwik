049792a8dd061b3d06d20dc6229b92504a16061d
Harsh Mangalam <harshdev8218@gmail.com>
2024-03-19 11:00:49 +0530

    added button ui

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,54 @@
+import { type QwikIntrinsicElements, component$, Slot } from "@builder.io/qwik";
+
+type Props = QwikIntrinsicElements["button"] & {
+  loading?: boolean;
+  fullWidth?: boolean;
+  circle?: boolean;
+  outline?: boolean;
+  roundedFull?: boolean;
+  size?: "btn-xs" | "btn-sm" | "btn-md" | "btn-lg";
+  btnClass?: any;
+  colorScheme?:
+    | "btn-neutral"
+    | "btn-primary"
+    | "btn-secondary"
+    | "btn-error"
+    | "btn-success"
+    | "btn-info"
+    | "btn-warning"
+    | "btn-ghost";
+};
+export const Button = component$((props: Props) => {
+  const {
+    loading,
+    fullWidth = false,
+    colorScheme = "",
+    size = "btn-md",
+    btnClass = null,
+    roundedFull = false,
+    circle = false,
+    outline = false,
+    ...rest
+  } = props;
+  return (
+    <button
+      disabled={loading}
+      class={[
+        "btn  transition-all duration-500",
+        colorScheme,
+        size,
+        { "btn-block": fullWidth },
+        { "rounded-full": roundedFull },
+        { "btn-circle": circle },
+        { "btn-outline": outline },
+        btnClass,
+      ]}
+      {...rest}
+      role="button"
+      aria-disabled={loading ? "true" : "false"}
+      aria-busy={loading ? "true" : "false"}
+    >
+      {loading ? <span class={`loading loading-dots`}></span> : <Slot />}
+    </button>
+  );
+});
