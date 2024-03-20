f1ce9ee3d4c6a34d56704828bee8b482f11aeeeb
Harsh Mangalam <harshdev8218@gmail.com>
2024-03-21 01:55:16 +0530

    Added form component to handle follow

---
+++ src/components/user/follow-action.tsx
@@ -0,0 +1,34 @@
+import { component$ } from "@builder.io/qwik";
+import { Form } from "@builder.io/qwik-city";
+import { Button } from "../ui/button";
+import { useFollowUser } from "~/shared/user";
+
+type FollowActionProps = {
+  id: string;
+  isFollowing: boolean;
+  shouldFollowBack: boolean;
+};
+export const FollowAction = component$(
+  ({ id, isFollowing, shouldFollowBack }: FollowActionProps) => {
+    const followUser = useFollowUser();
+    return (
+      <Form action={followUser} class="w-full">
+        <input type="hidden" name="userId" value={id} />
+        <Button
+          fullWidth
+          size="btn-sm"
+          colorScheme={isFollowing ? "btn-ghost" : "btn-neutral"}
+          loading={followUser.isRunning}
+          type="submit"
+          outline={isFollowing}
+        >
+          {isFollowing
+            ? "Unfollow"
+            : shouldFollowBack
+              ? "Follow back"
+              : "Follow"}
+        </Button>
+      </Form>
+    );
+  },
+);
--- src/components/user/user-suggestions/user-card.tsx
+++ src/components/user/user-suggestions/user-card.tsx
@@ -1,7 +1,7 @@
 import { component$ } from "@builder.io/qwik";
-import { Form } from "@builder.io/qwik-city";
-import { Button } from "~/components/ui/button";
-import { type UserSuggestionType, useFollowUser } from "~/shared/user";
+import { type UserSuggestionType } from "~/shared/user";
+import { FollowAction } from "../follow-action";
+import { Link } from "@builder.io/qwik-city";
 
 type UserCardProps = {
   user: Pick<
@@ -10,39 +10,33 @@ type UserCardProps = {
   >;
 };
 export const UserCard = component$(({ user }: UserCardProps) => {
-  const followUser = useFollowUser();
   return (
-    <article class="card bg-base-200">
-      <div class="card-body flex flex-col items-center gap-4 p-4">
-        <div class="avatar flex-none">
-          <div class="w-20 rounded-full">
-            <img src={user.image} alt={user.username} width={80} height={80} />
+    <Link href={`/${user.username}/`}>
+      <article class="card bg-base-200">
+        <div class="card-body flex flex-col items-center gap-4 p-4">
+          <div class="avatar flex-none">
+            <div class="w-20 rounded-full">
+              <img
+                src={user.image}
+                alt={user.username}
+                width={80}
+                height={80}
+              />
+            </div>
           </div>
-        </div>
 
-        <div class="flex flex-col gap-0">
-          <h3 class="font-semibold">{user.name}</h3>
-          <span class="opacity-60">{user.username}</span>
-        </div>
+          <div class="flex flex-col gap-0">
+            <h3 class="font-semibold">{user.name}</h3>
+            <span class="opacity-60">{user.username}</span>
+          </div>
 
-        <Form action={followUser} class="w-full">
-          <input type="hidden" name="userId" value={user.id} />
-          <Button
-            fullWidth
-            size="btn-sm"
-            colorScheme={user.isFollowing ? "btn-ghost" : "btn-neutral"}
-            loading={followUser.isRunning}
-            type="submit"
-            outline={user.isFollowing}
-          >
-            {user.isFollowing
-              ? "Unfollow"
-              : user.shouldFollowBack
-                ? "Follow back"
-                : "Follow"}
-          </Button>
-        </Form>
-      </div>
-    </article>
+          <FollowAction
+            id={user.id}
+            isFollowing={user.isFollowing}
+            shouldFollowBack={user.shouldFollowBack}
+          />
+        </div>
+      </article>
+    </Link>
   );
 });
