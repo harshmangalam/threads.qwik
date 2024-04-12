7ea800a711377e78c9063fb15ae56f27b1b032a5
Harsh Mangalam <harshdev8218@gmail.com>
2024-04-12 15:13:39 +0530

    show empty message for saved thread

--- src/routes/(app)/saved/index.tsx
+++ src/routes/(app)/saved/index.tsx
@@ -9,9 +9,15 @@ export default component$(() => {
 
   return (
     <div class="mt-4 grid grid-cols-1 gap-4">
-      {threads.value.map((thread) => (
-        <ThreadCard key={thread.id} thread={thread} />
-      ))}
+      {threads.value.length ? (
+        threads.value.map((thread) => (
+          <ThreadCard key={thread.id} thread={thread} />
+        ))
+      ) : (
+        <p class="text-center opacity-50">
+          Posts that you save will appear here.
+        </p>
+      )}
     </div>
   );
 });
