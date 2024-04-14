4531fbecb7d14de009fb71a6ae03b2bc3488a821
Harsh Mangalam <harshdev8218@gmail.com>
2024-04-14 11:49:42 +0530

    fix search input not sync with searchParams

--- src/routes/(app)/search/search-form.tsx
+++ src/routes/(app)/search/search-form.tsx
@@ -1,7 +1,9 @@
 import { component$ } from "@builder.io/qwik";
-import { Form } from "@builder.io/qwik-city";
+import { Form, useLocation } from "@builder.io/qwik-city";
 import SearchIcon from "~/assets/icons/search.svg?jsx";
 export const SearchForm = component$(() => {
+  const location = useLocation();
+  const defaultValue = location.url.searchParams.get("q");
   return (
     <Form class="join flex items-center">
       <input
@@ -9,6 +11,7 @@ export const SearchForm = component$(() => {
         name="q"
         class="input join-item input-bordered w-full"
         placeholder="Search"
+        value={defaultValue}
       />
       <button class="btn join-item">
         <SearchIcon class="h-6 w-6" />
