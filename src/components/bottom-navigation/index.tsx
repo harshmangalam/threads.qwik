1acb26fad10551f20c047ba6824d7956c229a461
Harsh Mangalam <harshdev8218@gmail.com>
2024-04-17 12:06:12 +0530

    fix spacing issues

--- src/components/bottom-navigation/index.tsx
+++ src/components/bottom-navigation/index.tsx
@@ -3,9 +3,11 @@ import { NavLinks } from "../navbar/nav-links";
 
 export const BottomNavigation = component$(() => {
   return (
-    <nav class="fixed bottom-0 z-40  flex h-16 w-full items-center bg-base-100 md:hidden ">
-      <div class="mx-auto w-full max-w-2xl px-4">
-        <NavLinks />
+    <nav class="fixed bottom-0 left-0 right-0 z-40 bg-base-100 py-3 md:hidden">
+      <div class="flex h-12 w-full items-center ">
+        <div class="mx-auto w-full max-w-2xl px-4">
+          <NavLinks />
+        </div>
       </div>
     </nav>
   );
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -5,8 +5,8 @@ import { NavDropdown } from "./nav-dropdown";
 
 export const Navbar = component$(() => {
   return (
-    <header class="fixed left-0 right-0 top-0 z-50 bg-base-100">
-      <nav class="container mx-auto h-16 w-full px-4 md:px-0">
+    <header class="fixed left-0 right-0 top-0 z-50 bg-base-100 py-3">
+      <nav class="container mx-auto h-12 w-full px-4 md:px-0">
         <div class="flex h-full items-center justify-between">
           <section class="flex flex-1 items-center justify-center md:flex-initial">
             <Logo />
