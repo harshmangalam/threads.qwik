89617e2a6bb351b9b3a2f42a62816c5009000f32
harshmangalam <harshdev8218@gmail.com>
2024-06-04 12:57:46 +0530

    added migrations

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,11 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+export const runtime = "edge";
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export const prisma = new PrismaClient();
